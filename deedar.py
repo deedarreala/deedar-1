268ae507316e52895a6fa77dc142ba66eca905b2
deedarreala <deedarbyallah@gmail.com>
2025-02-23 20:29:41 +0500

    Update deedar.py

--- deedar.py
+++ deedar.py
@@ -16,12 +16,12 @@ days = st.number_input("Enter Days to Search (1-30):", min_value=1, max_value=30
 
 # List of broader keywords
 keywords = [
- "Affair Relationship Stories", "Reddit Update", "Reddit Relationship Advice", "Reddit Relationship", 
-"Reddit Cheating", "AITA Update", "Open Marriage", "Open Relationship", "X BF Caught", 
-"Stories Cheat", "X GF Reddit", "AskReddit Surviving Infidelity", "GurlCan Reddit", 
+ "Esperienze Sessuali", "Tradimento", "Reddit Relationship Advice", "Reddit Relationship", 
+"Reddit Cheating", "Infedeltà", "Open Marriage", "Open Relationship", "X BF Caught", 
+"Stories Cheat", "Test e Rivelazioni", "Fenomeni Paranormali", "Paure e Regret", 
 "Cheating Story Actually Happened", "Cheating Story Real", "True Cheating Story", 
 "Reddit Cheating Story", "R/Surviving Infidelity", "Surviving Infidelity", 
-"Reddit Marriage", "Wife Cheated I Can't Forgive", "Reddit AP", "Exposed Wife", 
+"Fantasie e Perversioni", "Relazioni e Connessioni", "Reddit AP", "Dominanza e Sottomissione", 
 "Cheat Exposed"
 ]
 
